f25217b8ab6a73f70b51ced03faf92bfac30b62d
Hideyuki Tanaka <hide@MBA.local>
2020-09-04 10:47:51 +0900

    b

--- api/index.js
+++ api/index.js
@@ -1,15 +1,16 @@
 const express = require("express");
-const axios = require('axios');
+// const axios = require('axios');
 const app = express();
 
 app.get("/", function(req, res) {
-  // res.send("HelloWorld");
+  res.send("HelloWorld");
   // const api = req.query.api;
   // axios.get(`${api}`)
-  axios.get('https://qiita.com/api/v2/items?query=tag:javascript&per_page=1&page=1')
-    .then(response => {
-      res.send(response.data);
-    });
+
+  // axios.get('https://qiita.com/api/v2/items?query=tag:javascript&per_page=1&page=1')
+  //   .then(response => {
+  //     res.send(response.data);
+  //   });
 });
 
 module.exports = {
