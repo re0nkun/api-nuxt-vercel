176eab34c81683f38f214a9682167b9d59492a15
Hideyuki Tanaka <hide@MBA.local>
2020-09-05 10:56:00 +0900

    a

--- api/test.js
+++ api/test.js
@@ -1,25 +1,21 @@
-export default (req, res) => {
-  res.json({ name: 'John' });
-};
+const express = require("express");
+// const axios = require('axios');
+const app = express();
 
-// const express = require("express");
-// // const axios = require('axios');
-// const app = express();
+app.get("/", function(req, res) {
+  res.send({
+    message: 'Hello world!'
+  })
+  // const api = req.query.api;
+  // axios.get(`${api}`)
 
-// app.get("/", function(req, res) {
-//   res.send({
-//     message: 'Hello world!'
-//   })
-//   // const api = req.query.api;
-//   // axios.get(`${api}`)
+  // axios.get('https://qiita.com/api/v2/items?query=tag:javascript&per_page=1&page=1')
+  //   .then(response => {
+  //     res.send(response.data);
+  //   });
+});
 
-//   // axios.get('https://qiita.com/api/v2/items?query=tag:javascript&per_page=1&page=1')
-//   //   .then(response => {
-//   //     res.send(response.data);
-//   //   });
-// });
-
-// // module.exports = app
+module.exports = app
 // module.exports = {
 //   path: "/api",
 //   handler: app
