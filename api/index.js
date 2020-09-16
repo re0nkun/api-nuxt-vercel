f97e80dc821b6853af30dabd5399ed4716ac7d95
Hideyuki Tanaka <hide@MBA.local>
2020-09-16 21:46:13 +0900

    a

--- api/index.js
+++ api/index.js
@@ -3,6 +3,14 @@ const axios = require('axios');
 const app = express();
 
 app.get("/", function(req, res) {
+  res.setHeader('Access-Control-Allow-Credentials', true)
+  res.setHeader('Access-Control-Allow-Origin', '*')
+  res.setHeader('Access-Control-Allow-Methods', 'GET,OPTIONS,PATCH,DELETE,POST,PUT')
+  res.setHeader(
+    'Access-Control-Allow-Headers',
+    'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version'
+  )
+
   // Count
   const max_results = 30
 
